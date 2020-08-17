c9400a7dbc89e836f5b62d2e593f1adb05275c82
kimhg <kimhgedu@users.noreply.github.com>
2020-08-17 10:58:24 +0900

    [2020-08-17] 빌드 설정 파일, 빌드 패스

--- bigdata/src/components/AppFooter.vue
+++ bigdata/src/components/AppFooter.vue
@@ -1,6 +1,7 @@
 <template>
   <footer>
-    Copyleft :)
+    <span class="left">https://kimhg9511.github.io/bigdata_project/</span>
+    <span class="right">Copyleft :)</span>
   </footer>
 </template>
 
@@ -15,13 +16,19 @@ footer {
   flex-shrink: 0;
   font-family: "NanumBarunpen";
   height: 50px;
+  font-size: 1.2rem;
   color: #e5e5e5;
   font-weight: 900;
   background-color: #333;
-  text-align: right;
   line-height: 50px;
   padding-right: 20px;
   box-shadow: inset 0 3px 3px 0 rgba(255,255,255,0.15);
 }
-
+.left {
+  padding-left: 20px;
+  float: left;
+}
+.right {
+  float: right;
+}
 </style>
