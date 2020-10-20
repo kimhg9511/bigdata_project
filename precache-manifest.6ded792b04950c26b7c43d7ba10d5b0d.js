16dd85abde21f002db65358a7a767777b3ca3605
kimhg9511 <54794439+kimhg9511@users.noreply.github.com>
2020-10-21 01:10:46 +0900

    Deploy

---
+++ precache-manifest.6ded792b04950c26b7c43d7ba10d5b0d.js
@@ -0,0 +1,322 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c992e1085d404e7e5be245d433c0955",
+    "url": "/bigdata_project/_redirects"
+  },
+  {
+    "revision": "84332202090188baf63e",
+    "url": "/bigdata_project/css/app.cb2c8e19.css"
+  },
+  {
+    "revision": "f525eabc3ce8931e5cce",
+    "url": "/bigdata_project/css/chunk-0049d7fe.942d2fcf.css"
+  },
+  {
+    "revision": "ab88424c997248289afe",
+    "url": "/bigdata_project/css/chunk-1cd0dfa9.e615a4a0.css"
+  },
+  {
+    "revision": "dce8aaa17152e3a440c8",
+    "url": "/bigdata_project/css/chunk-37da4238.df792868.css"
+  },
+  {
+    "revision": "8f747a5a482403ca811c",
+    "url": "/bigdata_project/css/chunk-4333a3de.5c03f980.css"
+  },
+  {
+    "revision": "4bf4d7fdf9d5ad13be05",
+    "url": "/bigdata_project/css/chunk-53205de8.31162db3.css"
+  },
+  {
+    "revision": "2e1f2b019239a9258539",
+    "url": "/bigdata_project/css/chunk-5cd106cd.9fdd5164.css"
+  },
+  {
+    "revision": "3f38e3719e4561123fe5",
+    "url": "/bigdata_project/css/chunk-9650207e.033e1066.css"
+  },
+  {
+    "revision": "bd5e3f9499ac3c8a5790",
+    "url": "/bigdata_project/css/chunk-fd11e4c4.d67ecb8c.css"
+  },
+  {
+    "revision": "ae8171625d8466bbebb5ffad30db54c5",
+    "url": "/bigdata_project/data/each.csv"
+  },
+  {
+    "revision": "77ccf6a1931769737b8997106c1c1ba4",
+    "url": "/bigdata_project/data/ex3.html"
+  },
+  {
+    "revision": "c6cc9dce368f3fc8cd322a3be78d28d8",
+    "url": "/bigdata_project/data/hot_coin.json"
+  },
+  {
+    "revision": "944011cce0799ec5a1873ce900bedaba",
+    "url": "/bigdata_project/data/hot_coin_old.json"
+  },
+  {
+    "revision": "71af8a0c25156ca26a21d7c890db8281",
+    "url": "/bigdata_project/data/list_2.csv"
+  },
+  {
+    "revision": "edb93f08d967cce553c51cc2ee2711dc",
+    "url": "/bigdata_project/data/marcap.html"
+  },
+  {
+    "revision": "4406c53f5a960a5fa7de553d7cc7f5d5",
+    "url": "/bigdata_project/data/marcap08052.json"
+  },
+  {
+    "revision": "cef35a8d92c555f061cdf4356c8d40f9",
+    "url": "/bigdata_project/data/profit4.csv"
+  },
+  {
+    "revision": "8de49f8bb1220002880a5afce7a74853",
+    "url": "/bigdata_project/data/rank_1.csv"
+  },
+  {
+    "revision": "7f937bbdae59e63a6b3d0e9ee53bd579",
+    "url": "/bigdata_project/data/team_project.html"
+  },
+  {
+    "revision": "708b00d4fc28da553550d9bbf534209e",
+    "url": "/bigdata_project/data/team_project_old.html"
+  },
+  {
+    "revision": "99487060e552ad65fd6af9887135747f",
+    "url": "/bigdata_project/data/word.json"
+  },
+  {
+    "revision": "93724669754710640c1ccce6988be9d3",
+    "url": "/bigdata_project/data/word_old.json"
+  },
+  {
+    "revision": "1b3e17894570942f674e9375452b4e73",
+    "url": "/bigdata_project/favicons/android-icon-144x144.png"
+  },
+  {
+    "revision": "fbd8dd3dfa08ecb1866651f028adc722",
+    "url": "/bigdata_project/favicons/android-icon-192x192.png"
+  },
+  {
+    "revision": "b6b07adcb2d9b4dca16aa59ee96758dd",
+    "url": "/bigdata_project/favicons/android-icon-36x36.png"
+  },
+  {
+    "revision": "366ca7b61a403d654e45b715d1db5393",
+    "url": "/bigdata_project/favicons/android-icon-48x48.png"
+  },
+  {
+    "revision": "545b8dcdcd53adf95e42a64763daea3a",
+    "url": "/bigdata_project/favicons/android-icon-72x72.png"
+  },
+  {
+    "revision": "77e4ce784d5a83e0effa0e3ef0363616",
+    "url": "/bigdata_project/favicons/android-icon-96x96.png"
+  },
+  {
+    "revision": "4998a82f837c1db3d3ae49bfb94f3b2f",
+    "url": "/bigdata_project/favicons/apple-icon-114x114.png"
+  },
+  {
+    "revision": "4c1cf7b9cb38e7a8837eb5d88ce96343",
+    "url": "/bigdata_project/favicons/apple-icon-120x120.png"
+  },
+  {
+    "revision": "1b3e17894570942f674e9375452b4e73",
+    "url": "/bigdata_project/favicons/apple-icon-144x144.png"
+  },
+  {
+    "revision": "fd67e9eed34e63ee5e9f2bedd3ca295f",
+    "url": "/bigdata_project/favicons/apple-icon-152x152.png"
+  },
+  {
+    "revision": "f4eda3cc719d09fbf7e20981b8a90576",
+    "url": "/bigdata_project/favicons/apple-icon-180x180.png"
+  },
+  {
+    "revision": "8d761410d7d7335faffb2097dc38b96d",
+    "url": "/bigdata_project/favicons/apple-icon-57x57.png"
+  },
+  {
+    "revision": "146ab8789e44f516bdc2168ae14a1c72",
+    "url": "/bigdata_project/favicons/apple-icon-60x60.png"
+  },
+  {
+    "revision": "545b8dcdcd53adf95e42a64763daea3a",
+    "url": "/bigdata_project/favicons/apple-icon-72x72.png"
+  },
+  {
+    "revision": "b1e12676fce8cd8a7044a96be5362827",
+    "url": "/bigdata_project/favicons/apple-icon-76x76.png"
+  },
+  {
+    "revision": "20ef82d81c5905064b4a7c969aa5964c",
+    "url": "/bigdata_project/favicons/apple-icon-precomposed.png"
+  },
+  {
+    "revision": "20ef82d81c5905064b4a7c969aa5964c",
+    "url": "/bigdata_project/favicons/apple-icon.png"
+  },
+  {
+    "revision": "a28ca336d2f7b2117ffdc1745f493d99",
+    "url": "/bigdata_project/favicons/favicon-16x16.png"
+  },
+  {
+    "revision": "7d736408fb95c3c9fe30bd03df5e7651",
+    "url": "/bigdata_project/favicons/favicon-32x32.png"
+  },
+  {
+    "revision": "77e4ce784d5a83e0effa0e3ef0363616",
+    "url": "/bigdata_project/favicons/favicon-96x96.png"
+  },
+  {
+    "revision": "b58fcfa7628c9205cb11a1b2c3e8f99a",
+    "url": "/bigdata_project/favicons/manifest.json"
+  },
+  {
+    "revision": "1b3e17894570942f674e9375452b4e73",
+    "url": "/bigdata_project/favicons/ms-icon-144x144.png"
+  },
+  {
+    "revision": "8bfdd2e5dbc0df57187fd27bc8a9b9e2",
+    "url": "/bigdata_project/favicons/ms-icon-150x150.png"
+  },
+  {
+    "revision": "b1568a9df9a3b95b05ec369c124941b0",
+    "url": "/bigdata_project/favicons/ms-icon-310x310.png"
+  },
+  {
+    "revision": "c667c77b6796299641b37b372de7bfd2",
+    "url": "/bigdata_project/favicons/ms-icon-70x70.png"
+  },
+  {
+    "revision": "bafd78da573962d22a817cf22678c8dc",
+    "url": "/bigdata_project/fonts/KaushanScript-Regular.bafd78da.ttf"
+  },
+  {
+    "revision": "054ffd1ba6a6c109b3ccee195074f928",
+    "url": "/bigdata_project/fonts/Montserrat-Black.054ffd1b.ttf"
+  },
+  {
+    "revision": "a8de8ffe262db7e277deb8f1c439c518",
+    "url": "/bigdata_project/fonts/Montserrat-Bold.a8de8ffe.ttf"
+  },
+  {
+    "revision": "5e81217f7e141ed72533cde11d1252fa",
+    "url": "/bigdata_project/fonts/Montserrat-ExtraBold.5e81217f.ttf"
+  },
+  {
+    "revision": "b415dcea9b371c36eaf4a23e4fa1f5dc",
+    "url": "/bigdata_project/fonts/Montserrat-ExtraLight.b415dcea.ttf"
+  },
+  {
+    "revision": "9e52b00d737492e337b2498406aef98f",
+    "url": "/bigdata_project/fonts/Montserrat-Light.9e52b00d.ttf"
+  },
+  {
+    "revision": "5f797490f806b3b229299f0a66de89c9",
+    "url": "/bigdata_project/fonts/Montserrat-Medium.5f797490.ttf"
+  },
+  {
+    "revision": "f7213526ec9296ff43426bfe3eae8926",
+    "url": "/bigdata_project/fonts/Montserrat-Regular.f7213526.ttf"
+  },
+  {
+    "revision": "31e34a4dc526ffb104e2d988f6f52516",
+    "url": "/bigdata_project/fonts/Montserrat-SemiBold.31e34a4d.ttf"
+  },
+  {
+    "revision": "bbf2ff5d3749f144925150eee4c35caa",
+    "url": "/bigdata_project/fonts/Montserrat-Thin.bbf2ff5d.ttf"
+  },
+  {
+    "revision": "86bd0f8ad553a21319709f3cea42d9b7",
+    "url": "/bigdata_project/fonts/NanumBarunpenB.86bd0f8a.ttf"
+  },
+  {
+    "revision": "80d0c5f3a852beac9b0d2036d3d88845",
+    "url": "/bigdata_project/fonts/NanumBarunpenR.80d0c5f3.ttf"
+  },
+  {
+    "revision": "ecc61bfabe0637e8367a734e718a4f8e",
+    "url": "/bigdata_project/fonts/NanumSquareRoundB.ecc61bfa.ttf"
+  },
+  {
+    "revision": "00f73012c2e65950de3d7fa2be640c95",
+    "url": "/bigdata_project/fonts/NanumSquareRoundEB.00f73012.ttf"
+  },
+  {
+    "revision": "924b9ea5f0305f8dc6371fc4f12da37a",
+    "url": "/bigdata_project/fonts/NanumSquareRoundL.924b9ea5.ttf"
+  },
+  {
+    "revision": "4ba97a2a508f59611d45c41e7414ba66",
+    "url": "/bigdata_project/fonts/NanumSquareRoundR.4ba97a2a.ttf"
+  },
+  {
+    "revision": "55b70c6c8f174d1891db96953653d9fe",
+    "url": "/bigdata_project/img/logo_555.55b70c6c.png"
+  },
+  {
+    "revision": "eccf4239df8b42183f08868134511b83",
+    "url": "/bigdata_project/index.html"
+  },
+  {
+    "revision": "84332202090188baf63e",
+    "url": "/bigdata_project/js/app.f7cbaa16.js"
+  },
+  {
+    "revision": "f525eabc3ce8931e5cce",
+    "url": "/bigdata_project/js/chunk-0049d7fe.3b4f2134.js"
+  },
+  {
+    "revision": "ab88424c997248289afe",
+    "url": "/bigdata_project/js/chunk-1cd0dfa9.125b4b09.js"
+  },
+  {
+    "revision": "dce8aaa17152e3a440c8",
+    "url": "/bigdata_project/js/chunk-37da4238.a443cddf.js"
+  },
+  {
+    "revision": "8f747a5a482403ca811c",
+    "url": "/bigdata_project/js/chunk-4333a3de.a51e390b.js"
+  },
+  {
+    "revision": "4bf4d7fdf9d5ad13be05",
+    "url": "/bigdata_project/js/chunk-53205de8.829ce3d7.js"
+  },
+  {
+    "revision": "2e1f2b019239a9258539",
+    "url": "/bigdata_project/js/chunk-5cd106cd.c83e491f.js"
+  },
+  {
+    "revision": "cbece3ee62447a68a08f",
+    "url": "/bigdata_project/js/chunk-7bb3265c.205f4ece.js"
+  },
+  {
+    "revision": "3f38e3719e4561123fe5",
+    "url": "/bigdata_project/js/chunk-9650207e.58bc4d1f.js"
+  },
+  {
+    "revision": "bd5e3f9499ac3c8a5790",
+    "url": "/bigdata_project/js/chunk-fd11e4c4.a0504020.js"
+  },
+  {
+    "revision": "d3d38e81a3b4e6b6a42a",
+    "url": "/bigdata_project/js/chunk-vendors.1cb08183.js"
+  },
+  {
+    "revision": "2e553815fab9c1d9a309e7c49254575e",
+    "url": "/bigdata_project/manifest.json"
+  },
+  {
+    "revision": "39dff3077599e9c4a2d4120f1d58edbe",
+    "url": "/bigdata_project/reset.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bigdata_project/robots.txt"
+  }
+]);
